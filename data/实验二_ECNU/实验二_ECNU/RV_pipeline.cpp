ad889587f867696c4f62eea35ed8b191199fffcc
Flytortoise <3286114036@qq.com>
2021-10-20 18:45:27 +0800

    init experiment 2

--- data/实验二_ECNU/实验二_ECNU/RV_pipeline.cpp
+++ data/实验二_ECNU/实验二_ECNU/RV_pipeline.cpp
@@ -3,16 +3,7 @@
 #include<vector>
 #include<bitset>
 #include<fstream>
-#include <string>
-
-// #include "common.h"
-// #include "RISC_Instruction.h"
-// #include "RISC_INSMem.h"
-// #include "RISC_DataMem.h"
-// #include "RISC_Control.h"
-
 using namespace std;
-
 #define MemSize 1000 // memory size, in reality, the memory size should be 2^32, but for this lab, for the space resaon, we keep it as this large number, but the memory is still 32-bit addressable.
 
 struct IFStruct {
@@ -167,7 +158,7 @@ class DataMem
                     i++;
                 }
             }
-            else cout<<"Unable to open DataMem file";
+            else cout<<"Unable to open file";
                 dmem.close();          
         }
 		
@@ -259,13 +250,12 @@ void printState(stateStruct state, int cycle)
 }
  
 
-int main_pipeline()
+int main()
 {
     
     RF myRF;
     INSMem myInsMem;
     DataMem myDataMem;
-    stateStruct newState;
     struct stateStruct state{0};
     state.IF.nop = false;
     state.ID.nop = true;
@@ -274,10 +264,10 @@ int main_pipeline()
     state.WB.nop = true;
     state.EX.alu_op = true;
     int cycle = 0;
-
-    cout << "start pipline" << endl;
+			
+             
     while (1) {
-        break;
+
 
         /* --------------------- WB stage --------------------- */
 
@@ -331,78 +321,5 @@ int main_pipeline()
     myRF.outputRF(); // dump RF;	
     myDataMem.outputDataMem(); // dump data mem 
 
-    return 0;
-}
-
-
-// int main_risc_ins(int argc, char *argv[])
-// {
-//     if (argc != 2) {
-//         RISC_DEBUG::COUT("Please input file path !");
-//         return -1;
-//     }
-
-//     string filePath(argv[1]);
-//     RISC_DEBUG::COUT("input file path:", filePath);
-//     INSMem myInsMem(string(filePath).append(INS_INFILE));
-//     DataMem myDataMem(string(filePath).append(DATE_INFILE),string(filePath).append(DATE_OUTFILE));
-
-//     RISC_Control *risc_control = RISC_Control::GetInstance();
-
-//     bitset<32> PC;
-
-//     while (1) {
-//         RISC_DEBUG::COUT("start PC:", to_string(PC.to_ulong()));
-//         // 1. Fetch Instruction
-//         bitset<32> instruction = myInsMem.ReadMemory(PC);
-//         RISC_Instruction *ins_obj = risc_control->GetINSObject(instruction);
-//         if (ins_obj != nullptr){
-//             ins_obj->print();
-//         }else {
-//             RISC_DEBUG::COUT("Instruction is :", instruction.to_string());
-//             if (instruction.to_ulong() == 0xffffffff) {
-//                 RISC_DEBUG::COUT("this is 'Halt instruction', save data mem to dmemresult.txt and exit simulator");
-//                 break;
-//             } else {
-//                 RISC_DEBUG::COUT("FAILED: instruction is incorrect, eixt !");
-//                 break;
-//             }
-//         }
-
-//         // 2. Register File Instruction
-//         risc_control->RF_Func();
-
-//         // 3. Execuete alu operation
-//         risc_control->ALU_Func();
-
-//         // 4. Read/Write Mem(Memory Access)
-//         risc_control->DataMem_Func(myDataMem);
-
-//         // 5. Register File Update(Write Back)
-//         risc_control->RF_Func_back();
-
-//         // Update pc
-//         if (risc_control->GetCurrentINSEm() == EM_BEQ && 
-//             risc_control->GetCurrentALUResult().to_ulong() == 0) {
-//             PC = bitset<32>(PC.to_ulong() + risc_control->GetBEQImm().to_ulong());
-//         } else if (risc_control->GetCurrentINSEm() == EM_JAL) {
-//             PC = bitset<32>(PC.to_ulong() + 4 + risc_control->GetCurrentALUResult().to_ulong());
-//         } else {
-//             PC = bitset<32>(PC.to_ulong() + 4);
-//         }
-//         RISC_DEBUG::COUT("after update PC:", to_string(PC.to_ulong()));
-
-//         RISC_RF_Data::OutputRF(filePath);
-//     }
-
-//     myDataMem.OutputDataMem(); // dump data memX
-//     return 0;
-// }
-
-
-int main(int argc, char *argv[])  {
-    // main_risc_ins(argc, argv);
-    main_pipeline();
-    
     return 0;
 }
